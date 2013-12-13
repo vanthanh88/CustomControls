98913b0bc9464c2c261fdf903f85816862a08d83
Ghost Developer <PhuMac@PhuMacs-Mac-mini.local>
2013-12-13 11:22:02 +0700

    Initial Commit

---
+++ CustomControls/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomControls
+//
+//  Created by PhuMac on 12/13/13.
+//  Copyright (c) 2013 PhuMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
